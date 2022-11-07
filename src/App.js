775471d8880637bf2c776d1dcbd286a230911cea
Grzegorz Szymański <grszyman@gmail.com>
2022-11-07 22:04:40 +0100

    Hi Adzia!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! WoW... <code>docker-compose up</code> works!
+          Hi Adzia!
         </p>
         <a
           className="App-link"
